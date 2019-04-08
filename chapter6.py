8a283498b37cb5901a059d3d27d4ba3ddc0ebbcc
Tiger1422 <2002taika@gmail.com>
2019-04-08 13:41:00 +0100

    温度と時間

--- chapter6.py
+++ chapter6.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-  
+
 import smbus
 import time
 import datetime  #課題2用
-import json  #課題2用
+import json  #課題2
+
+log={}
 
 i2c = smbus.SMBus(1)
 address = 0x48
@@ -14,7 +16,15 @@ for i in range(10):
     temp = (block[0] << 8 | block[1]) >> 3
     if(temp >= 4096):
         temp -= 8192
+    print(datetime.datetime.now())
+    log["id"+str(i)]={}
+    log["id"+str(i)]["time"]=str(datetime.datetime.now())
     print("Temperature:%6.2f" % (temp / 16.0))
-    time.sleep(1)
+    log["id"+str(i)]["temp"]=str(temp)
+time.sleep(1)
+
+
+
+json.dump(log, json_file)
 
 json_file.close()
